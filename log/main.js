a20a26c049a13122075f9f5469240abb23bc39be
Letalandroid <carlossoncra@protonmail.com>
2021-12-16 09:50:53 -0500

    Add files via upload

--- log/main.js
+++ log/main.js
@@ -6,3 +6,46 @@ document.getElementById("exit").addEventListener("click", () => {
     localStorage.clear();
 
 });
+
+let numTarea = 0;
+
+document.getElementById("add").addEventListener("click", () => {
+
+    let tarea = document.createElement("li");
+    let content = document.getElementById("tarea").value;
+    tarea.innerHTML = content;
+
+    localStorage.setItem(`tarea${numTarea}`, content);    
+
+    document.getElementById("tareas").insertAdjacentHTML("beforeend", `<li>${localStorage.getItem(`tarea${numTarea}`)}</li>`);
+
+    numTarea++;
+
+});
+
+document.addEventListener("keydown", (e) => {
+
+    if (e.key == "Enter") {
+
+        e.preventDefault();
+        document.getElementById("add").click();
+        document.getElementById("tarea").value = "";
+
+    }
+
+});
+
+document.getElementById("clear").addEventListener("click", () => {
+
+    document.getElementById("tarea").value = "";
+    document.getElementById("tareas").innerHTML = "";
+
+    while (numTarea >= 0) {
+
+        numTarea--;
+        localStorage.removeItem(`tarea${numTarea}`);
+
+    }
+
+
+});
